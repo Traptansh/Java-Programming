ba600320649c36e4d1566468e1b2dac58d61a8f6
Traptansh <76931926+Traptansh@users.noreply.github.com>
2021-01-28 14:28:33 +0530

    Add files via upload

--- method.java
+++ method.java
@@ -1,16 +1,29 @@
-public class method { 
+public class method { // class
+    // we cannot access anything inside a class without a object
     public static void fun() {
-        System.out.println("Function.");
+        System.out.println("Hi i am Function.");
+
     }
 
-    public void students() {
-        String student1 = "shubham";
-        String student2 = "pratik";
-        String student3 = "aman";
-        System.out.println("students are:-" + student1 + " " + student2 + " " + student3);
+    public void cheaters() {// no arguments & void returntype
+        String Cheater1 = "Deepmita";
+        String Cheater2 = "Sachin";
+        String Cheater3 = "Kadam";
+        System.out.println("Cheater's are:-" + Cheater1 + " " + Cheater2 + " " + Cheater3);
     }
 
-    public int sum() {
+    /*
+     * types of function 1) takes nothing & returns nothing(with void returntype &
+     * no arguments) 2) takes something returns nothing(taking parameters void
+     * returntype) 3)takes nothing returns something(no argument & returntype is not
+     * void) 4)takes something returns something
+     */
+    public void number(int a) {// taking parameter & returning nothing
+        a++;// 11
+        System.out.println("Function type is :-Takes Something & returns nothing" + a);// 6
+    }
+
+    public int sum() {// takes nothing returns integer
         int a, b;
         a = 2;
         b = 3;
@@ -21,13 +34,24 @@ public class method {
         return z;
     }
 
-    public static void main(String[] args) { //takes nothing returns something
-        method o1 = new method();
+    public static void main(String[] args) { // method
+        // function:- piece of code which is called when we need it.
+        // function definition
+        // function call
+        /*
+         * syntax:- returntype function_name(pass arguments){ block of code }
+         * function_name();
+         */
+        method o1 = new method();// ignore this
         fun();
-        o1.students();
+        o1.cheaters();
+        o1.cheaters();
+        o1.cheaters();
+        o1.number(10);
         int result = o1.sum();
-        System.out.println("Takes nothing returns something" + result);
+        System.out.println("Function type is takes nothing returns something" + result);
         char store = o1.alphabet('a');
         System.out.println(store);
+
     }
 }
--- pconstructor.java
+++ pconstructor.java
@@ -1,8 +1,9 @@
+package oops;
+
 public class pconstructor {
-    pconstructor(int i) {
-        System.out.println(i);
- }
+        pconstructor(int i) {
+            System.out.println(i);
+        }
 public static void main(String[] args) {
-        pconstructor o1 = new pconstructor(10);
-     }
- }
+    pconstructor 
+}
